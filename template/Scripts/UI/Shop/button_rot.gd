c2b5f85f405acfc06e84ecd5d6d332c976fcdaf3
adam-r96 <coolrockgaming@gmail.com>
2025-11-29 14:35:28 +0000

    button and rot button changes

--- template/Scripts/UI/Shop/button_rot.gd
+++ template/Scripts/UI/Shop/button_rot.gd
@@ -2,12 +2,6 @@ extends ButtonBase
 
 @export var rotate_right = false
 
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-
-
 func _on_pressed() -> void:
 	if rotate_right == true:
 		GVar.signal_bus.rotate_cam_right.emit()
