5e60c511284110b7bc0edd8d62726514f222bb74
adam-r96 <coolrockgaming@gmail.com>
2025-11-29 00:57:06 +0000

    ui stuff

--- template/Scripts/Resources/GameResources/ShipResources/rocket_part_resource.gd
+++ template/Scripts/Resources/GameResources/ShipResources/rocket_part_resource.gd
@@ -3,3 +3,4 @@ class_name RocketPartResource extends Resource
 @export var scene_path_string:String = "null"
 @export var icon:CompressedTexture2D = null
 @export var cost:int = 0
+@export var mass:float = 1.0
